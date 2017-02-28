65bcbbd826376b3dc11d4e77a599f5be49eb522b
Brian Brennan <me@brianbrennan.com>
2017-02-28 16:24:43 -0500

    simplification

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: __dirname + '/app/index.html',
     filename: 'index.html',
     inject: 'body'
-})
+});
 
 module.exports = {
     entry: {
